d39437fe196b060f28f2a412f3777e8e1dd4d881
trakimobile <igorvilar@outlook.com>
2016-04-27 14:07:15 -0300

    Create label with text "Hello Word".

--- Hello Word/ViewController.swift
+++ Hello Word/ViewController.swift
@@ -9,11 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var textHome: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        NSLog("TESTE");
+        textHome.text = "Hello Word";
     }
 
     override func didReceiveMemoryWarning() {
