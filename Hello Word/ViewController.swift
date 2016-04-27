888778d4c227eb1a5f13499a3a2dfbf7f3c93828
trakimobile <igorvilar@outlook.com>
2016-04-27 12:46:43 -0300

    Add files project.

---
+++ Hello Word/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Hello Word
+//
+//  Created by Igor Vilar on 4/27/16.
+//  Copyright © 2016 Traki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        NSLog("TESTE");
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
